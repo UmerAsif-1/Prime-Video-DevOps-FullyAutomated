55f0a4e57f944aca8fd72a1b591869ce4dddb659
umerasif <umer.asif@student.oulu.fi>
2025-02-23 11:39:54 +0200

    creating variable.tf

--- Terraform/Ec2server/variable.tf
+++ Terraform/Ec2server/variable.tf
@@ -8,3 +8,28 @@ variable "ec2_sg_name" {
     description = "Name of the security group"
     type = string
 }
+
+variable "ami_id" {
+    description = "AMI ID"
+    type = string
+}
+variable "instance_type" {
+    description = "Instance type"
+    type = string
+}
+variable "key_name" {    
+    description = "Key name"
+    type = string
+}
+variable "volume_size" {
+    description = "Volume size"
+    type = number
+}
+variable "instance_name" {
+    description = "Instance name"
+    type = string
+}
+variable "private_key_path" {
+    description = "Private key path"
+    type = string
+} 
